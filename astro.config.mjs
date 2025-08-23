641217ff9a890caf1adc9f15d533827dcf808734
Paguiel <119151331+paguielng@users.noreply.github.com>
2025-08-23 20:23:04 +0200

    Updated astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://shoppis-wiki.netlify.app',
+  site: 'https://shopisdev.netlify.app',
 });
